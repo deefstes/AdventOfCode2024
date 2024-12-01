e818946157544146f990c7fad1a345ee41b4feb8
Dewald Swanepoel <dewald.swanepoel@gmail.com>
2024-12-01 23:11:12 +0200

    Day 1

---
+++ AdventOfCode2024/Day01/Solver.cs
@@ -0,0 +1,41 @@
+﻿using AdventOfCode2024.Utils;
+
+namespace AdventOfCode2024.Day01
+{
+    public class Solver(string input) : ISolver
+    {
+        private readonly string _input = input;
+
+        public string Part1()
+        {
+            var col1 = GetColumn(0).Order().ToList();
+            var col2 = GetColumn(1).Order().ToList();
+
+            var total = 0;
+            for (int i = 0; i < col1.Count(); i++)
+            {
+                total += Math.Abs(col2[i] - col1[i]);
+            }
+            return total.ToString();
+        }
+
+        public string Part2()
+        {
+            var col1 = GetColumn(0);
+            var col2 = GetColumn(1);
+
+            var total = 0;
+            for (int i = 0; i < col1.Count(); i++)
+            {
+                var multiplier = col2.Where(x => x == col1[i]).Count();
+                total += col1[i] * multiplier;
+            }
+            return total.ToString();
+        }
+
+        public List<int> GetColumn(int col)
+        {
+            return _input.AsList().Select(i => int.Parse(i.Split(' ', StringSplitOptions.RemoveEmptyEntries)[col])).ToList();
+        }
+    }
+}
---
+++ AdventOfCode2024Tests/Day01Tests.cs
@@ -0,0 +1,27 @@
+﻿using AdventOfCode2024.Day01;
+using NUnit.Framework;
+
+namespace AdventOfCode2024.Tests.Day01
+{
+    [TestFixture()]
+    public class SolverTests
+    {
+        [Test()]
+        public void Part1Test()
+        {
+            Solver solver = new();
+            var rsp = solver.Part1(File.ReadAllText($"Day01\\sample1.txt"));
+
+            Assert.That(rsp, Is.EqualTo("11"));
+        }
+
+        [Test()]
+        public void Part2Test()
+        {
+            Solver solver = new();
+            var rsp = solver.Part2(File.ReadAllText($"Day01\\sample1.txt"));
+
+            Assert.That(rsp, Is.EqualTo("31"));
+        }
+    }
+}
